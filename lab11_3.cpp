fb461286aedda4997c38e833174413099fbe4b85
PlubCPE <chakkapop_s@cmu.ac.th>
2021-12-17 17:28:03 +0700

    lab11_3_complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,37 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<string>
+#include <iomanip>
+#include<cstdlib>
+
+using namespace std;
+
+
+
+int main()
+{
+    int num = 0;
+    double mean,devi, sum = 0, sum2 = 0;
+    string copy;
+    ifstream score("score.txt");
+    while(getline(score,copy))
+	{
+        
+        sum += atof(copy.c_str());
+        sum2 += pow((atof(copy.c_str())),2);
+        num++; 
+
+	}
+
+    
+
+    cout << "Number of data = " << num << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << (mean = (sum/num)) << endl;
+    cout << "Standard deviation = " << (devi = sqrt((sum2/num) - pow(mean,2)));
+
+    score.close();
+    return 0;
+}
+
